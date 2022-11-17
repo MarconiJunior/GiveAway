ee64af9592e191078c7230a9d6820f8094f46e7d
MarconiJunior <junior@brumafe.com>
2022-11-17 18:30:03 -0300

    FirstCommit

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+
+let resultado = document.getElementById("resultado")
+resultado.style.border = "0px";
+function toReset() {
+  let min = document.getElementById("num1");
+  let max = document.getElementById("num2");
+  max = Number.parseInt(max.value)
+  min = Number.parseInt(min.value)
+  let sort = Math.floor(Math.random() * ((max + 1) - min) + min);
+  if (min > max) {
+    window.alert("O primeiro número deve ser maior que o segundo!!!")
+  } else {
+    setTimeout(() => {
+      resultado.innerHTML = sort
+    }, "500")
+  }
+}
+
